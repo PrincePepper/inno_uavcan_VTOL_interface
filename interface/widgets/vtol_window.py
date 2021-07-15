b317760bd11816bbac673e2443e8ae54894db577
GinormousSalmon <terleczcky.alex@gmail.com>
2021-07-15 13:32:07 +0300

    data fields placed correctly

--- interface/widgets/vtol_window.py
+++ interface/widgets/vtol_window.py
@@ -30,6 +30,32 @@ def render_vendor_specific_status_code(s):
     return out
 
 
+def make_vbox(*widgets, stretch_index=None, s=1):
+    box = QVBoxLayout()
+    for idx, w in enumerate(widgets):
+        if stretch_index is not None:
+            box.addStretch(stretch_index[idx])
+        box.addWidget(w)
+    box.addStretch(s)
+    box.setContentsMargins(0, 0, 0, 0)
+    container = QWidget()
+    container.setLayout(box)
+    container.setContentsMargins(0, 0, 0, 0)
+    return container
+
+
+def make_hbox(*widgets, stretch_index=None):
+    box = QHBoxLayout()
+    for idx, w in enumerate(widgets):
+        box.addWidget(w, 0 if stretch_index is None else stretch_index[idx])
+    box.addStretch(1)
+    box.setContentsMargins(0, 0, 0, 0)
+    container = QWidget()
+    container.setLayout(box)
+    container.setContentsMargins(0, 0, 0, 0)
+    return container
+
+
 class NodeBlock(QDialog):
     def __init__(self, name: str):
         super().__init__()
@@ -37,33 +63,11 @@ class NodeBlock(QDialog):
         self.fields = [QLabel('Status:', self), QLabel('Data:', self)]
         self.data = [QLabel('0', self), QLabel('0', self)]
 
-    def make_vbox(self, *widgets, stretch_index=None):
-        box = QVBoxLayout(self)
-        for idx, w in enumerate(widgets):
-            box.addWidget(w, 1 if idx == stretch_index else 0)
-        box.addStretch(1)
-        box.setContentsMargins(0, 0, 0, 0)
-        container = QWidget(self)
-        container.setLayout(box)
-        container.setContentsMargins(0, 0, 0, 0)
-        return container
-
-    def make_hbox(self, *widgets, stretch_index=None):
-        box = QHBoxLayout(self)
-        for idx, w in enumerate(widgets):
-            box.addWidget(w, 1 if idx == stretch_index else 0)
-        box.addStretch(1)
-        box.setContentsMargins(0, 0, 0, 0)
-        container = QWidget(self)
-        container.setLayout(box)
-        container.setContentsMargins(0, 0, 0, 0)
-        return container
-
-    def get_widget(self, stretch1, stretch2):
-        vlay = QVBoxLayout(self)
+    def get_widget(self, stretch1=0, stretch2=0):
+        vlay = QHBoxLayout(self)
         vlay.addStretch(stretch1)
-        box = self.make_vbox(self.label,
-                             self.make_hbox(self.make_vbox(*self.fields), self.make_vbox(*self.data)))
+        box = make_vbox(self.label,
+                        make_hbox(make_vbox(*self.fields), make_vbox(*self.data)))
         box.setStyleSheet("background-color:#ffffff;")
         box.setContentsMargins(5, 5, 5, 5)
         vlay.addWidget(box)
@@ -130,17 +134,41 @@ class VtolWindow(QDialog):
             motor2 = NodeBlock("motor2")
             motor3 = NodeBlock("motor3")
             motor4 = NodeBlock("motor4")
+            aileron1 = NodeBlock("aileron1")
+            aileron2 = NodeBlock("aileron2")
+            aileron3 = NodeBlock("aileron3")
+            aileron4 = NodeBlock("aileron4")
+            aileron5 = NodeBlock("aileron5")
+            gps = NodeBlock("gps")
+            airspeed = NodeBlock("airspeed")
+            pressure = NodeBlock("pressure")
+            engine = NodeBlock("engine")
 
             layout = QHBoxLayout(self)
-            layout.addStretch(4)
-            layout.addWidget(motor1.get_widget(1, 2))
+
+            box1 = make_vbox(aileron1.get_widget(), aileron3.get_widget(), stretch_index=[1, 1], s=3)
+            box2 = make_vbox(aileron4.get_widget(), motor2.get_widget(), stretch_index=[0, 1], s=6)
+            box3 = make_vbox(aileron5.get_widget(), engine.get_widget(), stretch_index=[0, 1], s=10)
+            box4 = make_vbox(motor4.get_widget(), gps.get_widget(), motor1.get_widget(), stretch_index=[2, 2, 3], s=6)
+            box5 = make_vbox(airspeed.get_widget(), stretch_index=[1], s=1)
+            box6 = make_vbox(aileron2.get_widget(), pressure.get_widget(), stretch_index=[1, 2, 1], s=3)
+            box7 = make_vbox(motor3.get_widget(), stretch_index=[1], s=2)
+
+            layout.addStretch(8)
+            layout.addWidget(box1)
             layout.addStretch(2)
-            layout.addWidget(motor2.get_widget(1, 4))
+            layout.addWidget(box2)
+            layout.addStretch(2)
+            layout.addWidget(box3)
             layout.addStretch(1)
-            layout.addWidget(motor3.get_widget(3, 1))
-            layout.addStretch(3)
-            layout.addWidget(motor4.get_widget(1, 1))
-            layout.addStretch(1)
+            layout.addWidget(box4)
+            layout.addStretch(2)
+            layout.addWidget(box5)
+            layout.addStretch(0)
+            layout.addWidget(box6)
+            layout.addStretch(2)
+            layout.addWidget(box7)
+            layout.addStretch(2)
 
             self._control_widget.setAlignment(Qt.AlignRight)
             self._control_widget.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
